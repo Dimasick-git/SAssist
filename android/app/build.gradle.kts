91f78a11541406c21a1e18aaea3310d05c3dc922
Dimasick-git <dimaorynchukk@gmail.com>
2026-08-12 06:24:41 +0000

    fix: migrate Android JVM compiler options

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -47,8 +47,10 @@ android {
         targetCompatibility = JavaVersion.VERSION_17
         isCoreLibraryDesugaringEnabled = true
     }
-    kotlinOptions {
-        jvmTarget = "17"
+    kotlin {
+        compilerOptions {
+            jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17)
+        }
     }
     buildFeatures {
         compose = true
